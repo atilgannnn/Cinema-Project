62f1e6ec4f46d1e2d5cc3ad7979990ada4416f47
atilgansahinulasan <a.sahinulasan@gmail.com>
2023-04-09 02:02:18 +0300

    "Cinema-Project"

--- src/main/java/com/runners/cinema/dto/noNameDTO.java
+++ src/main/java/com/runners/cinema/dto/noNameDTO.java
@@ -1,4 +1,15 @@
 package com.runners.cinema.dto;
 
-public class nonameDTO {
+public class noNameDTO {
+
+    public static void main(String[] args) {
+
+        /* Bu class sadece deneme amaçlı olarak oluşturulmuştur */
+
+
+    }
+
+
+
+
 }
