0bdddc8bd2d9fabaa0d47d929fd8839f0cd2ec0a
atilgannnn <85996994+atilgannnn@users.noreply.github.com>
2023-04-11 15:19:54 +0300

    Add files via upload

---
+++ src/main/java/com/runners/cinema/dto/ReservationDTO.java
@@ -0,0 +1,118 @@
+package com.runners.cinema.model.enums;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public class ReservationCDTO {
+
+    private Long id;
+
+    private Long userId;
+
+    private Long tickedId;
+
+    private LocalDateTime reservationDate;
+
+    private AudienceStatus audienceStatus;
+
+    private Set<Long> seat;
+
+    private String pnrNumber;
+
+    private Long movieId;
+
+    private Long cinemaId;
+
+    //construct
+
+
+    public ReservationCDTO(Long id, Long userId, Long tickedId, LocalDateTime reservationDate, AudienceStatus audienceStatus, Set<Long> seat, String pnrNumber, Long movieId, Long cinemaId) {
+        this.id = id;
+        this.userId = userId;
+        this.tickedId = tickedId;
+        this.reservationDate = reservationDate;
+        this.audienceStatus = audienceStatus;
+        this.seat = seat;
+        this.pnrNumber = pnrNumber;
+        this.movieId = movieId;
+        this.cinemaId = cinemaId;
+    }
+
+    public ReservationCDTO() {
+    }
+
+    //Getter and Setter
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getTickedId() {
+        return tickedId;
+    }
+
+    public void setTickedId(Long tickedId) {
+        this.tickedId = tickedId;
+    }
+
+    public LocalDateTime getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(LocalDateTime reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public AudienceStatus getAudienceStatus() {
+        return audienceStatus;
+    }
+
+    public void setAudienceStatus(AudienceStatus audienceStatus) {
+        this.audienceStatus = audienceStatus;
+    }
+
+    public Set<Long> getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Set<Long> seat) {
+        this.seat = seat;
+    }
+
+    public String getPnrNumber() {
+        return pnrNumber;
+    }
+
+    public void setPnrNumber(String pnrNumber) {
+        this.pnrNumber = pnrNumber;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+
+    public Long getCinemaId() {
+        return cinemaId;
+    }
+
+    public void setCinemaId(Long cinemaId) {
+        this.cinemaId = cinemaId;
+    }
+}
